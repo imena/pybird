faa9cce6ff7e97e033135418b970ec03ce555b47
Roman Bulakh <bulah.roman@gmail.com>
2023-09-23 21:31:37 +0200

    add route type parsing

--- pybird/__init__.py
+++ pybird/__init__.py
@@ -7,7 +7,7 @@ from subprocess import PIPE, Popen
 
 class PyBird:
     # BIRD reply codes: https://github.com/CZ-NIC/bird/blob/6c11dbcf28faa145cfb7310310a2a261fd4dd1f2/doc/reply_codes
-    ignored_field_numbers = (0, 1, 13, 1008, 2002, 9001)
+    ignored_field_numbers = (0, 1, 13, 2002, 9001)
     error_fields = (13, 19, 8001, 8002, 8003, 9000, 9001, 9002)
     success_fields = (0, 3, 4, 18, 20)
 
@@ -368,6 +368,20 @@ class PyBird:
                 return []
 
         return routes
+    
+    def _parse_route_type(self, line):
+        """
+        Parse a line like:
+        1008-	Type: BGP unicast univ
+        1008-	Type: device unicast univ
+        """
+        match = re.match(r".+Type: (?P<type>\w+) (?P<scope>\w+) (?P<family>\w+)", line)
+        if not match:
+            raise ValueError(f"couldn't parse line '{line}'")
+
+        route_type = match.groupdict()
+
+        return route_type
 
     def _re_route_summary(self):
         return re.compile(
